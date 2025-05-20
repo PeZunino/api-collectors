ad944d4f489c14e66ba574de052a2dccdc351cc9
PeZunino <pedrozsantos@hotmail.com>
2025-05-20 18:01:50 -0300

    chore(core): correct eslint / typescript config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,25 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
-import config from '@pezunino/eslint-config'
+import js from '@eslint/js';
+import config from '@pezunino/eslint-config';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.node } },
-  tseslint.configs.recommended,
-  ...config
+	{
+		files: [
+			'**/*.{js,mjs,cjs,ts,mts,cts}'
+		],
+		plugins: { js },
+		extends: [
+			'js/recommended'
+		] 
+	},
+	{
+		files: [
+			'**/*.{js,mjs,cjs,ts,mts,cts}'
+		],
+		languageOptions: { globals: globals.node } 
+	},
+	tseslint.configs.recommended,
+	...config
 ]);
