04ac267c012288480cae02df028017c5e0e903ff
PeZunino <pedrozsantos@hotmail.com>
2025-05-20 18:03:09 -0300

    feat(auctioneer): auctioneer entity

---
+++ src/domain/entities/auctioneer.ts
@@ -0,0 +1,121 @@
+import { Address } from '../valueObjects/address';
+import { Email } from '../valueObjects/email';
+import { PhoneNumber } from '../valueObjects/phone';
+import { UniqueID } from '../valueObjects/uniqueId';
+import { Website } from '../valueObjects/website';
+
+interface Props{
+	name: string
+	registrationCode: string
+	phoneNumber: PhoneNumber
+	email: Email
+	websites: Website[]
+	address:Address
+}
+
+type AddressProps = typeof Address.prototype;
+
+export class Auctioneer{
+	private readonly id:UniqueID;
+
+	private constructor(private props:Props, id?:UniqueID){
+		this.id = id ?? new UniqueID();
+	}
+
+	public static create(props:Omit<Props,'websites'> & {websiteURLs:string[]}){
+		const email = Email.create(props.email.address); 
+
+		const phoneNumber = PhoneNumber.create(props.phoneNumber.rawNumber); 
+
+		const address = Address.create({
+			cep: props.address.cep,
+			city: props.address.city,
+			neighborhood: props.address.neighborhood,
+			number: props.address.number,
+			state: props.address.state,
+			street: props.address.street,
+		});
+
+		const websites = props.websiteURLs.map(url => Website.create(url)); 
+    
+		return new Auctioneer({
+			address,
+			email,
+			name:props.name,
+			phoneNumber,
+			registrationCode:props.registrationCode,
+			websites
+		});
+	}
+
+	public equals(other: Auctioneer): boolean {
+		if (!(other instanceof Auctioneer)) {
+			return false;
+		}
+
+		return this.id.equals(other.id);
+	}
+  
+	public changeName(newName: string): void {
+		this.props.name = newName;
+	}
+
+	public updateContactInfo(newEmail: string, newPhoneNumber: string): void {
+		this.props.email = Email.create(newEmail);
+
+		this.props.phoneNumber = PhoneNumber.create(newPhoneNumber);
+	}
+
+	public changeAddress(newAddressProps:AddressProps): void {
+		this.props.address = Address.create(newAddressProps);
+	}
+
+	public addWebsite(url: string): void {
+		const newWebsite = Website.create(url);
+
+		if (!this.props.websites.some(w => w.equals(newWebsite))) {
+			this.props.websites.push(newWebsite);
+		}
+	}
+
+	public removeWebsite(url: string): void {
+		try {
+			const websiteToRemove = Website.create(url);
+
+			this.props.websites = this.props.websites.filter(w => !w.equals(websiteToRemove));
+		} catch {
+			console.warn(`Attempted to remove invalid URL: ${url}`);
+		}
+	}
+
+	get idValue(): UniqueID {
+		return this.id;
+	}
+
+	get name(): string {
+		return this.props.name;
+	}
+
+	get registrationCode(): string {
+		return this.props.registrationCode;
+	}
+
+	get phoneNumber(): PhoneNumber {
+		return this.props.phoneNumber;
+	}
+
+	get email(): Email {
+		return this.props.email;
+	}
+
+	get websites(): readonly Website[] {
+		return [
+			...this.props.websites
+		];
+	}
+
+	get address(): Address {
+		return this.props.address;
+	}
+
+}
